ea99c9e2396d48f2869d8487e683159183d1c21f
Elene Qaldani <126418804+EleneQ@users.noreply.github.com>
2023-11-01 22:41:36 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import "./components/cardsList.css";
   openSpots={item.openSpots}
 
   we could also pass into the card item={item}, so pass in 
-  an individual data element object, but then we're have to use
+  an individual data element object, but then we'd have to use
   the .item in the card component code, so that would clatter 
   it a bit
 */
